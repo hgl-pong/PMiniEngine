6e583104b16db736e7e60d8016bd5b4b2c22fb24
国灵 何 <2319100642@qq.com>
2023-07-03 20:27:56 +0800

    Model

--- include/Core/HModel.h
+++ include/Core/HModel.h
@@ -2,18 +2,20 @@
 #define HMODEL_H
 #include "HMath.h"
 #include "../../include/HEngine.h"
+
 struct HVertex
 {
-    HVector3f position;
-    HVector3f normal;
-    HVector4f tangent;
-    std::vector<HVector2f> texcoord;
+    HVector3f position;              // 顶点位置
+    HVector3f normal;                // 顶点法线
+    HVector4f tangent;               // 顶点切线
+    std::vector<HVector2f> texcoord; // 顶点纹理坐标
 };
-class Model:public IHModel_Base
+
+class Model : public IHModel_Base
 {
 public:
-    std::vector<HVertex> m_Vertices;
-    std::vector<unsigned short> m_Indice16;
-    std::vector<unsigned long> m_Indice32;
+    std::vector<HVertex> m_Vertices;        // 顶点数据
+    std::vector<unsigned short> m_Indice16; // 16 位索引数据
+    std::vector<unsigned long> m_Indice32;  // 32 位索引数据
 };
 #endif // HMODEL_H
