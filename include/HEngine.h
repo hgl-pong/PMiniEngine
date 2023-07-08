468d5652af3b1b7b269e3274774d79d395a99a0d
国灵 何 <2319100642@qq.com>
2023-07-08 21:17:43 +0800

    Engine

--- include/HEngine.h
+++ include/HEngine.h
@@ -1,8 +1,38 @@
 #ifndef HENGINE_H
 #define HENGINE_H
+#include <windows.h>
+#include <HMath.h>
+#define API __declspec(dllexport)
+
+class IHEngine;
+class IHResourceMananger;
+class IHActor;
+class IHModel_Base;
+class IHMaterial;
+class IHMaterialSystem;
+class IHScene;
+class IHComponent;
+class IHSceneView;
+
+class IHActor
+{
+public:
+}
+
+class IHEngine
+{
+public:
+    HRESULT GetReSourceManager(IHResourceMananger **resourceManager) = 0;
+    HRESULT CreateActorFromFile(const char *path, IHActor **actor) = 0;
+}
+
 class IHModel_Base
 {
 public:
 };
+namespace Utils
+{
+    HRESULT API GetEngine(IHEngine **engine);
+}
 
 #endif // HENGINE_H
